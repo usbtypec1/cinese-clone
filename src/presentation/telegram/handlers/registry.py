592622954d878bc3daea800165dd666004a62d2f
Eldos <eldos.baktybekov@gmail.com>
2025-09-15 05:58:09 +0600

    Add help router to the list of routers in registry

--- src/presentation/telegram/handlers/registry.py
+++ src/presentation/telegram/handlers/registry.py
@@ -7,10 +7,12 @@ from presentation.telegram.handlers.start import (
 )
 from presentation.telegram.handlers.advertisement import advertisement_router
 from presentation.telegram.handlers.register_user import register_user_router
+from presentation.telegram.handlers.help import help_router
 
 
 def get_routers() -> tuple[Router, ...]:
     return (
+        help_router,
         register_user_router,
         admin_start_router,
         user_start_router,
